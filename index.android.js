04e11cfb1faedac28c26c7c83eea6a3741281fcc
Emanuele <Emanuele.d>
2017-11-02 10:46:24 +0000

    live video support

--- index.android.js
+++ index.android.js
@@ -5,167 +5,189 @@ const { RNChromeCast } = NativeModules;
 
 export default RNChromeCast;*/
 
-
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
-    requireNativeComponent,
-    ViewPropTypes,
-    findNodeHandle,
-    NativeModules,
-    DeviceEventEmitter,
-    Text,
-    View,
-    Dimensions
+  requireNativeComponent,
+  ViewPropTypes,
+  findNodeHandle,
+  NativeModules,
+  DeviceEventEmitter,
+  Text,
+  View,
+  Dimensions
 } from 'react-native';
 
 let NativeChromeCast = NativeModules.RNChromeCast;
 
-
 function ChromeCastPlayer(available) {
+  let isAvailable = available;
 
-    let isAvailable = available;
-
-    let runIfAvailable = function (func) {
-        if (isAvailable) {
-             return func().then(() => Promise.resolve(this))
-        } else {
-          return Promise.reject(new Error('Player not available.'))
-        }
-    };
-
-    let setUnavailable = function () {
-        isAvailable = false;
-        return Promise.resolve(this)
-    };
-
-
-    this.play = () => {
-        return runIfAvailable(() => NativeChromeCast.play())
-    };
-
-    this.pause = () => {
-        return runIfAvailable(() => NativeChromeCast.pause())
-    };
-
-    this.stop = () => {
-        return runIfAvailable(() => NativeChromeCast.stop().then(() => setUnavailable()))
+  let runIfAvailable = function(func) {
+    if (isAvailable) {
+      return func().then(() => Promise.resolve(this));
+    } else {
+      return Promise.reject(new Error('Player not available.'));
     }
+  };
 
+  let setUnavailable = function() {
+    isAvailable = false;
+    return Promise.resolve(this);
+  };
 
+  this.play = () => {
+    return runIfAvailable(() => NativeChromeCast.play());
+  };
+
+  this.pause = () => {
+    return runIfAvailable(() => NativeChromeCast.pause());
+  };
+
+  this.stop = () => {
+    return runIfAvailable(() =>
+      NativeChromeCast.stop().then(() => setUnavailable())
+    );
+  };
 }
 
 function ChromeCastWrapper() {
-    this.loadVideo = (video) => {
-        return NativeChromeCast.loadVideo(video.url, video.title, video.subtitle,
-            video.image, video.duration, video.isLive, video.mimeType)
-		  .then(() => NativeChromeCast.start(video.progress).then(() => new ChromeCastPlayer(true)))
-    };
+  this.loadVideo = video => {
+    return NativeChromeCast.loadVideo(
+      video.url,
+      video.title,
+      video.subtitle,
+      video.image,
+      video.duration,
+      video.isLive,
+      video.mimeType
+    ).then(() =>
+      NativeChromeCast.start(video.progress).then(
+        () => new ChromeCastPlayer(true)
+      )
+    );
+  };
 
-    this.showChromeCastActivity = (cb) => {
-        NativeChromeCast.showChromeCastActivity()
-    }
+  this.loadLiveVideo = video => {
+    return NativeChromeCast.loadVideo(
+      video.url,
+      video.title,
+      video.subtitle,
+      video.image,
+      0,
+      true,
+      video.mimeType
+    ).then(() =>
+      NativeChromeCast.start(0).then(() => new ChromeCastPlayer(true))
+    );
+  };
+
+  this.showChromeCastActivity = cb => {
+    NativeChromeCast.showChromeCastActivity();
+  };
 }
 
-export let ChromeCast = new ChromeCastWrapper()
-
+export let ChromeCast = new ChromeCastWrapper();
 
 function ChromeCastScanner() {
-    let listenForSessionEvents = (cb) => {
-        return DeviceEventEmitter.addListener("ChromeCastSessionEvent", (e) => {
-            cb({status: e.SESSION_STATUS, message: e.SESSION_STATUS_MESSAGE})
-        })
-    }
+  let listenForSessionEvents = cb => {
+    return DeviceEventEmitter.addListener('ChromeCastSessionEvent', e => {
+      cb({ status: e.SESSION_STATUS, message: e.SESSION_STATUS_MESSAGE });
+    });
+  };
 
-    let listenForScanEvent = (cb) => {
-        return DeviceEventEmitter.addListener("ChromeCastScanEvent", (e) => {
-            cb({
-                deviceConnected: e.DEVICE_CONNECTED,
-                deviceConnecting: e.DEVICE_CONNECTING,
-                devicesAvailable: e.DEVICES_AVAILABLE
-            })
-        })
-    }
+  let listenForScanEvent = cb => {
+    return DeviceEventEmitter.addListener('ChromeCastScanEvent', e => {
+      cb({
+        deviceConnected: e.DEVICE_CONNECTED,
+        deviceConnecting: e.DEVICE_CONNECTING,
+        devicesAvailable: e.DEVICES_AVAILABLE
+      });
+    });
+  };
 
-    this.setUp = () => {
-        NativeChromeCast.setUpScanner();
-    }
+  this.setUp = () => {
+    NativeChromeCast.setUpScanner();
+  };
 
-    this.startScan = (scanCb, sessionCb) => {
-        NativeChromeCast.startScan();
-        this.scanListener = listenForScanEvent(scanCb);
-        this.sessionListener = listenForSessionEvents(sessionCb)
-    }
+  this.startScan = (scanCb, sessionCb) => {
+    NativeChromeCast.startScan();
+    this.scanListener = listenForScanEvent(scanCb);
+    this.sessionListener = listenForSessionEvents(sessionCb);
+  };
 
-    this.stopScan = () => {
-	  this.scanListener.remove();
-	  this.sessionListener.remove()
-	  NativeChromeCast.stopScan();
-	}
+  this.stopScan = () => {
+    this.scanListener.remove();
+    this.sessionListener.remove();
+    NativeChromeCast.stopScan();
+  };
 }
 
-let NativeChromeCastButton = requireNativeComponent('RCTChromeCastButton', ChromeCastButton);
+let NativeChromeCastButton = requireNativeComponent(
+  'RCTChromeCastButton',
+  ChromeCastButton
+);
 
 export class ChromeCastButton extends Component {
+  constructor(props) {
+    super(props);
+    this.scanner = new ChromeCastScanner();
+  }
 
-    constructor(props) {
-        super(props);
-        this.scanner = new ChromeCastScanner();
-    }
+  render() {
+    return <NativeChromeCastButton {...this.props} />;
+  }
 
-    render() {
-        return <NativeChromeCastButton {...this.props} />;
-    }
+  componentWillMount() {
+    this.scanner.setUp();
+    let { onScanEventReceived, onSessionEventReceived } = this.props;
+    this.scanner.startScan(onScanEventReceived, onSessionEventReceived);
+  }
 
-    componentWillMount() {
-        this.scanner.setUp()
-        let {onScanEventReceived, onSessionEventReceived} = this.props;
-        this.scanner.startScan(onScanEventReceived, onSessionEventReceived);
-    }
-
-    componentWillUnmount() {
-        this.scanner.stopScan();
-    }
+  componentWillUnmount() {
+    this.scanner.stopScan();
+  }
 }
 
 ChromeCastButton.propTypes = {
-    onScanEventReceived: PropTypes.func.isRequired,
-    onSessionEventReceived: PropTypes.func.isRequired,
-    ...ViewPropTypes
-}
+  onScanEventReceived: PropTypes.func.isRequired,
+  onSessionEventReceived: PropTypes.func.isRequired,
+  ...ViewPropTypes
+};
 
 ChromeCastButton.defaultProps = {
-    onScanEventReceived: () => {
-    },
-    onSessionEventReceived: () => {
-    },
-}
+  onScanEventReceived: () => {},
+  onSessionEventReceived: () => {}
+};
 
 export let SessionStatus = Object.freeze({
-    STARTING: 0,
-    STARTED: 1,
-    START_FAILED: 2,
-    ENDING: 3,
-    ENDED: 4,
-    RESUMING: 5,
-    RESUMED: 6,
-    RESUME_FAILED: 7,
-    SUSPENDED: 8,
-})
+  STARTING: 0,
+  STARTED: 1,
+  START_FAILED: 2,
+  ENDING: 3,
+  ENDED: 4,
+  RESUMING: 5,
+  RESUMED: 6,
+  RESUME_FAILED: 7,
+  SUSPENDED: 8
+});
 
-
-let NativeChromeCastMiniController = requireNativeComponent('RCTChromeCastMiniController', ChromeCastMiniController);
+let NativeChromeCastMiniController = requireNativeComponent(
+  'RCTChromeCastMiniController',
+  ChromeCastMiniController
+);
 
 export class ChromeCastMiniController extends Component {
-
-    render() {
-        return <View>
-            <NativeChromeCastMiniController {...this.props} />
-        </View>;
-    }
+  render() {
+    return (
+      <View>
+        <NativeChromeCastMiniController {...this.props} />
+      </View>
+    );
+  }
 }
 
 ChromeCastMiniController.propTypes = {
-    progress: PropTypes.number,
-    indeterminate: PropTypes.bool,
-    ...ViewPropTypes
-}
+  progress: PropTypes.number,
+  indeterminate: PropTypes.bool,
+  ...ViewPropTypes
+};
